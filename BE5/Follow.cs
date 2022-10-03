e36547cfe0814d07d0f86aa2ffa40f276eed4910
이희범 <71597554+lhb00@users.noreply.github.com>
2022-10-03 12:31:27 +0900

    B38. 플레이어 이동

---
+++ BE5/Follow.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Follow : MonoBehaviour
+{
+    public Transform target; // 따라갈 목표와 위치 오프셋을 public 변수로 선언
+    public Vector3 offset;
+
+    void Update()
+    {
+        transform.position = target.position + offset;
+    }
+}
